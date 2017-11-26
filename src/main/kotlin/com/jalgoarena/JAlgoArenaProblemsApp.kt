e0232a8b8e5381a0d3a44f0205108e5cb96bcfdf
Mariusz Sowa <mariuszsowa11@gmail.com>
2017-11-26 18:33:36 +0100

    fix swagger

--- src/main/kotlin/com/jalgoarena/JAlgoArenaProblemsApp.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaProblemsApp.kt
@@ -6,6 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
@@ -15,6 +19,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 open class JAlgoArenaProblemsApp {
     @Bean
     open fun restTemplate() = RestTemplate()
+
+
+    @Bean
+    open fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+    }
 }
 
 fun main(args: Array<String>) {
